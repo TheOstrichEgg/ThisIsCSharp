cff62e32060b99706d77aeba56bfc7b1305c9936
YongJoon Kim <ostricheggyj@gmail.com>
2021-04-23 22:32:53 +0900

    Fix #2
    Hello World!!!

---
+++ chapter_01/Hello/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        // Program entry point
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Use this like : Hello.exe <name>");
+                return;
+            }
+
+            WriteLine("Hello, {0}", args[0]);
+        }
+    }
+}
