ffff1d2ab93216487e892b547c26ae345b6b1189
YongJoon Kim <ostricheggyj@gmail.com>
2021-04-23 23:20:00 +0900

    Ongoing #3
     - IntegralTypes
     - SignedUnsigned
     - Overflow
     - FloatingPoint
     - Decimal

---
+++ chapter_03/Decimal/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Decimal
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            float   a = 3.1415_9265_3589_7932_3846f;
+            double  b = 3.1415_9265_3589_7932_3846;
+            decimal c = 3.1415_9265_3589_7932_3846m;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
