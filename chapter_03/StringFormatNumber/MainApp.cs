f9e5a8bd54db35878fa7d24b5ed1943ec15be0aa
YongJoon Kim <ostricheggyj@gmail.com>
2021-04-24 23:11:43 +0900

    Close #3 Yeahy~ 😎
    - Nullable
    - var
    - Common type system
    - string

---
+++ chapter_03/StringFormatNumber/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace StringFormatNumber
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // D : Decimal
+            WriteLine("Decimal: {0:D}", 123);
+            WriteLine("Decimal: {0:D5}", 123);
+
+            // X : HeaxaDecimal
+            WriteLine("Decimal: 0x{0:X}", 0xFF1234);
+            WriteLine("Decimal: 0x{0:X}", 0xFF1234);
+
+            // N : Number
+            WriteLine("Number: {0:N}", 123456789);
+            WriteLine("Number: {0:N0}", 123456789);
+
+            // F : Fixed Point
+            WriteLine("Fixed Point: {0:F}", 123.45);
+            WriteLine("Fixed Point: {0:F5}", 123.456);
+
+            // E : Engineering
+            WriteLine("Engineering: {0:E}", 123.456789);
+        }
+    }
+}
---
+++ chapter_03/StringInterpolation/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace StringInterpolation
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string name = "KTT";
+            int age = 23;
+            WriteLine($"{name,-10}, {age:D3}");
+
+            name = "PNS";
+            age = 30;
+            WriteLine($"{name}, {age,-10:D3}");
+
+            name = "LBS";
+            age = 17;
+            WriteLine($"{name}, {(age > 20 ? "Majority" : "Minority")}");
+        }
+    }
+}
