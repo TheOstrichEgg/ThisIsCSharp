da5357fcdc37864843b2b1f6e69e6807585dee23
YongJoon Kim <ostricheggyj@gmail.com>
2021-04-25 20:33:00 +0900

    Resolve #4 Operator!!!
    - ArithmaticOperator
    - IncDecOperator
    - StringConcatenate
    - RealtionalOperator
    - LogicalOperator
    - ConditionalOperator
    - NullConditionalOperator
    - ShiftOperator
    - BitwiseOperator
    - AssignmentOperator
    - NullCoalescing

---
+++ chapter_04/ConditionalOperator/MainApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConditionalOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string result = (10 % 2) == 0 ? "Even" : "Odd";
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ chapter_04/NullConditionalOperator/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using static System.Console;
+
+namespace NullConditionalOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("BaseBall");
+            a?.Add("Soccer");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList();
+            a?.Add("BaseBall");
+            a?.Add("Soccer");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+        }
+    }
+}
