c8ad52eb02fe97d657720704256f37b4e3f7c183
YongJoon Kim <ostricheggyj@gmail.com>
2021-04-25 20:43:37 +0900

    Ongoing #5 😎
    - IfElse
    - IfIf

---
+++ chapter_05/IfElse/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfElse
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Number : ");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+                Console.WriteLine("Negative");
+            else if (number > 0)
+                Console.WriteLine("Positive");
+            else
+                Console.WriteLine("0");
+
+            if (number % 2 == 0)
+                Console.WriteLine("Even number");
+            else
+                Console.WriteLine("Odd number");
+        }
+    }
+}
---
+++ chapter_05/IfIf/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfIf
+{
+    internal class MainApp
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Please Input a number : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number > 0)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("Positive even number");
+                else
+                    Console.WriteLine("Positive odd number");
+            }
+            else
+            {
+                Console.WriteLine("0 or negative number");
+            }
+        }
+    }
+}
