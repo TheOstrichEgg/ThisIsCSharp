222ff8956398310e068eaf2ddf38e128d14e6854
YongJoon Kim <ostricheggyj@gmail.com>
2021-04-28 00:01:56 +0900

    Ongoing #6 Method 😎
    - method and return
    - call by value / ref

---
+++ chapter_06/Return/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Return
+{
+    class MainApp
+    {
+        static int Fibonacci(int n)
+        {
+            if (n < 2)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        static void PrintProfile(string name, string phone)
+        {
+            if (name == "")
+            {
+                Console.WriteLine("Please enter your name.");
+                return;
+            }
+
+            Console.WriteLine($"Name: {name}, Phone: {phone}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"10th number of Fibonacci number: {Fibonacci(10)}");
+
+            PrintProfile("", "123-456");
+            PrintProfile("PSH", "123-456");
+        }
+    }
+}
