09ba8317908944e8ff14dc8050af46b19554a26d
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-01 01:00:32 +0900

    Ongoing #7 Class 👨‍💻
    - BasicClass / Constructor / StaticField

---
+++ chapter_07/BasicClass/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : Meow");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.Color = "White";
+            kitty.Name = "Kitty";
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat();
+            nero.Color = "Black";
+            nero.Name = "Nero";
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
---
+++ chapter_07/Constructor/MainApp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Cat
+    {
+        public Cat()
+        {
+            Name = "";
+            Color = "";
+        }
+
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : Bye~");
+        }
+
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : Meow");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat("Kitty", "White");
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat("nero", "Black");
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
