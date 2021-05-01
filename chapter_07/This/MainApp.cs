2c68f8481fdf707af899aa487ccb0b215da3aff3
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-01 16:59:55 +0900

    Ongoing #7 Class 👨‍💻
    - DeepCopy / This / ThisConstructor
    - AccessModifier / Inheritance

---
+++ chapter_07/This/MainApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace This
+{
+    class Employee
+    {
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPostion(string Position)
+        {
+            this.Position = Position;
+        }
+
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh");
+            pooh.SetPostion("Waiter");
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+            Employee tiger = new Employee();
+            tiger.SetName("Tiger");
+            tiger.SetPostion("Cleaner");
+            Console.WriteLine($"{tiger.GetName()} {tiger.GetPosition()}");
+        }
+    }
+}
