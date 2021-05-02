266768bbc38ca975ad21c166829bcc40d627c7d9
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-02 12:39:37 +0900

    Ongoing #7 Class 👨‍💻
    - TypeCasting
    - ReadOnlyFields
    - Overriding / SealedMethod / MethodHiding
    - NestedClass / PartialClass

---
+++ chapter_07/SealedMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SealedMethod
+{
+    class Base
+    {
+        public virtual void SealMe()
+        {
+        }
+    }
+
+    class Derived : Base
+    {
+        public sealed override void SealMe()
+        {
+        }
+    }
+
+    class WantToOverride : Derived
+    {
+        public override void SealMe()
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
