631dae39c10049983fcd7af7e18220be95aaa302
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-05 00:58:11 +0900

    Ongoing #9 Property 🤔
     - Property, AutoImplementProperty
     - ConstructWithProperty, InitOnly

---
+++ chapter_09/AutoImplementProperty/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AutoImplementProperty
+{
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "Unknown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Name: {birth.Name}");
+            Console.WriteLine($"Birthday: {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age: {birth.Age}");
+
+            BirthdayInfo birth = new BirthdayInfo();
+            birth.Name = "Michael Jackson";
+            birth.Birthday = new DateTime(1958, 8, 29);
+
+            Console.WriteLine($"Name: {birth.Name}");
+            Console.WriteLine($"Birthday: {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age: {birth.Age}");
+        }
+    }
+}
---
+++ chapter_09/ConstructWithProperty/MainApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConstructWithProperty
+{
+    class BirthdayInfo
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo()
+            {
+                Name = "Michael Jackson",
+                Birthday = new DateTime(1958, 8, 29)
+            };
+
+            Console.WriteLine($"Name: {birth.Name}");
+            Console.WriteLine($"Birthday: {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age: {birth.Age}");
+        }
+    }
+}
