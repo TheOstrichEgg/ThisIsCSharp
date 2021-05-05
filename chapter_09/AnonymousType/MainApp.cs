55ce6ce5f2322dd2c0f85b7316872bd743002728
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-05 18:34:21 +0900

    Resolve #9 👨‍💻
    - Record, Record Comp
    - WithExp, Anonymous Type
    - Properties In Interface, Properties In Abstract Class

---
+++ chapter_09/AnonymousType/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var a = new { Name = "Michael Jackson", Age = 20 };
+            Console.WriteLine($"Name: {a.Name}, Age: {a.Age}");
+
+            var b = new { Subject = "Math", Scores = new int[] { 90, 80, 70, 60 } };
+            Console.Write($"Subject: {b.Subject}, Scores:");
+            foreach (var score in b.Scores)
+                Console.Write($"{score} ");
+        }
+    }
+}
---
+++ chapter_09/PropertiesInInterface/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PropertiesInInterface
+{
+    interface INamedValue
+    {
+        string Name { get; set; }
+        string Value { get; set; }
+    }
+
+    class NamedValue : INamedValue
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} : {Value}";
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            NamedValue name   = new NamedValue() { Name = "Name",   Value = "Bob Marley" };
+            NamedValue height = new NamedValue() { Name = "height", Value = "180cm" };
+            NamedValue weight = new NamedValue() { Name = "weight", Value = "70Kg" };
+
+            // Modify book example
+            Console.WriteLine(name);
+            Console.WriteLine(height);
+            Console.WriteLine(weight);
+        }
+    }
+}
