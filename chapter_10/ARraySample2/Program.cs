6b531826275e7e87fa1e35fd64d48d49e71ab311
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-05 20:13:06 +0900

    Ongoing #10 Array 🤔
     - Array Samples, Initializing Array
     - Derived from Array
     - More on Array

---
+++ chapter_10/ARraySample2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ARraySample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            scores[0] = 80;
+            scores[1] = 74;
+            scores[2] = 81;
+            scores[^2] = 90;
+            scores[^1] = 34;
+
+            foreach (int score in scores)
+                Console.WriteLine(score);
+
+            int Sum = 0;
+            foreach (int score in scores)
+                Sum += score;
+
+            int average = Sum / scores.Length;
+            Console.WriteLine($"Average Score: {average}");
+        }
+    }
+}
---
+++ chapter_10/InitializingArray/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InitializingArray
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[3] { "Saluton", "Hello", "Halo" };
+            Console.WriteLine("array1...");
+            foreach (string greeting in array1)
+                Console.WriteLine($" {greeting}");
+
+            string[] array2 = new string[] { "Saluton", "Hello", "Halo" };
+            Console.WriteLine("\narray2...");
+            foreach (string greeting in array2)
+                Console.WriteLine($" {greeting}");
+
+            string[] array3 = { "Saluton", "Hello", "Halo" };
+            Console.WriteLine("\narray3...");
+            foreach (string greeting in array3)
+                Console.WriteLine($" {greeting}");
+        }
+    }
+}
