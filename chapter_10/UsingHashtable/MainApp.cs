f9e19ff5c41f8be4c8a0b5b474bb0251c000e19a
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-07 23:59:01 +0900

    Resolve #10 yeah~ 😁
     - Slice
     - 2DArray / 3DArray / JaggedArray
     - Using Collections (List / Queue / Stack / Hashtable) /  InitializingCollections
     - Indexer / Yield / Enumerable

---
+++ chapter_10/UsingHashtable/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace UsingHashtable
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht["first"]  = "one";
+            ht["second"] = "two";
+            ht["third"]  = "three";
+            ht["fourth"] = "four";
+            ht["fifth"]  = "five";
+
+            Console.WriteLine(ht["first"]);
+            Console.WriteLine(ht["second"]); 
+            Console.WriteLine(ht["third"]); 
+            Console.WriteLine(ht["fourth"]);
+            Console.WriteLine(ht["fifth"]); 
+        }
+    }
+}
