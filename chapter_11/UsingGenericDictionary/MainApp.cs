ae042963c3b852b5c0944379bf27d20874d66117
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-19 23:09:35 +0900

    Resolve #11 yeah~ 😁
     - Copying Array / Generic / ConstraintsOnTypeParameters
     - UsingGeneric List / Queue / Stack / Dictionary
     - EnumerableGeneric

---
+++ chapter_11/UsingGenericDictionary/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingGenericDictionary
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            dic["One"]   = "First";
+            dic["Two"]   = "Second";
+            dic["Three"] = "Third";
+            dic["Four"]  = "Fourth";
+            dic["Five"]  = "Fifth";
+
+            Console.WriteLine(dic["One"]);
+            Console.WriteLine(dic["Two"]);
+            Console.WriteLine(dic["Three"]);
+            Console.WriteLine(dic["Four"]);
+            Console.WriteLine(dic["Five"]);
+        }
+    }
+}
---
+++ chapter_11/UsingGenericStack/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingGenericStack
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> queue = new Stack<int>();
+
+            queue.Push(1);
+            queue.Push(2);
+            queue.Push(3);
+            queue.Push(4);
+            queue.Push(5);
+
+            while (queue.Count > 0)
+                Console.WriteLine(queue.Pop());
+        }
+    }
+}
