3490b13ecc8dd91b22bb3cfcb1abc0b2e48d416a
YongJoon Kim <ostricheggyj@gmail.com>
2021-05-25 00:16:34 +0900

    resolve #12 KillingProgram TryCath / Throw / ThrowExpression / Finally MyException / ExceptionFiltering StackTrace

---
+++ chapter_12/Finally/MainApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Finally
+{
+    class MainApp
+    {
+        static int Divide(int dividend, int divisor)
+        {
+            try
+            {
+                Console.WriteLine("Divide() Start");
+                return dividend / divisor;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Divide() error occured");
+                throw e;
+            }
+            finally{
+                Console.WriteLine("Divide() end");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Input dividend: ");
+                String temp = Console.ReadLine();
+                int dividend = Convert.ToInt32(temp);
+
+                Console.Write("Input divisor: ");
+                temp = Console.ReadLine();
+                int divisor = Convert.ToInt32(temp);
+
+                Console.WriteLine("{0}/{1} = {2}",
+                    dividend, divisor, Divide(dividend, divisor));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program terminated...");
+            }
+        }
+    }
+}
---
+++ chapter_12/TryCatch/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1, 2, 3};
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"Error occured: {e.Message}");
+            }
+
+            Console.WriteLine("Program End");
+        }
+    }
+}
