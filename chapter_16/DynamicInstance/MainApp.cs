a7de970fd67c2b3893200f589fe2bbdd50686164
YongJoon Kim <ostricheggyj@gmail.com>
2021-06-15 01:27:29 +0900

    Ongoing #16 Reflection ?????
    GetType, DynamicInstance
    EmitTest

---
+++ chapter_16/DynamicInstance/MainApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynamicInstance
+{
+    class Profile
+    {
+        private string name;
+        private string phone;
+        public Profile()
+        {
+            name = "";
+            phone = "";
+        }
+
+        public Profile(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{name}, {phone}");
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("DynamicInstance.Profile");
+            MethodInfo methodInfo = type.GetMethod("Print");
+
+            PropertyInfo nameProperty = type.GetProperty("Name");
+            PropertyInfo phoneProperty = type.GetProperty("Phone");
+
+            object profile = Activator.CreateInstance(type, "Michael Jackson", "123-4567");
+            methodInfo.Invoke(profile, null);
+
+            nameProperty.SetValue(profile, "Freddie Mercury", null);
+            phoneProperty.SetValue(profile, "234-5678", null);
+
+            Console.WriteLine("{0}, {1}",
+                nameProperty.GetValue(profile, null),
+                phoneProperty.GetValue(profile, null));
+        }
+    }
+}
