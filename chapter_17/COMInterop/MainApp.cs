391e5c5fb626e9ef85ddcad7e4ad8d18c5b4de08
YongJoon Kim <ostricheggyj@gmail.com>
2021-06-22 22:25:45 +0900

    Resolve #17 Dynamic
    DuckTypeing
    COMInterop, WithPython

---
+++ chapter_17/COMInterop/MainApp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace COMInterop
+{
+    class MainApp
+    {
+        public static void OldWay(string[,] data, string savepath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+
+            excelApp.Workbooks.Add(Type.Missing);
+
+            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                ((Excel.Range)workSheet.Cells[i + 1, 1]).Value2 = data[i, 0];
+                ((Excel.Range)workSheet.Cells[i + 1, 2]).Value2 = data[i, 1];
+            }
+
+            workSheet.SaveAs(savepath + "\\old.xlsx",
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing);
+
+            excelApp.Quit();
+        }
+
+        public static void NewWay(string[,] data, string savePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+
+            excelApp.Workbooks.Add();
+
+            Excel._Worksheet workSheet = (Excel._Worksheet)excelApp.ActiveSheet;
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                ((Excel.Range)workSheet.Cells[i + 1, 1]).Value2 = data[i, 0];
+                ((Excel.Range)workSheet.Cells[i + 1, 2]).Value2 = data[i, 1];
+            }
+
+            workSheet.SaveAs(savePath + "\\dynamic.xlsx");
+            excelApp.Quit();
+        }
+
+        static void Main(string[] args)
+        {
+            string savePath = System.IO.Directory.GetCurrentDirectory();
+            string[,] array = new string[,]
+            {
+                { "Michael Jackson", "Thriller" },
+                { "Pink Floyd", "The Dark Side of The Moon" },
+                { "Prince", "Purple Rain" },
+                { "Queen", "A Night at The Opera" }
+            };
+
+            Console.WriteLine("Creating Excel document in old way...");
+            OldWay(array, savePath);
+
+            Console.WriteLine("Creating Excel document in new way...");
+            NewWay(array, savePath);
+        }
+    }
+}
