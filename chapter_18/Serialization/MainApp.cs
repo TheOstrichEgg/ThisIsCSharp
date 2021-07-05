afc7f5c502fa97dfc0665e60382a0701d733f178
YongJoon Kim <ostricheggyj@gmail.com>
2021-07-05 23:56:05 +0900

    Resolve #18 File Handling
    Dir, Touch
    Basic IO, SeqNRand
    Using Declaration
    Binary File, Text File
    Serialization, Serializing Collection

---
+++ chapter_18/Serialization/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    [Serializable]
+    class NameCard
+    {
+        public string Name;
+        public string Phone;
+        public int Age;
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            using (Stream ws = new FileStream("a.dat", FileMode.Create))
+            {
+                BinaryFormatter serializer = new BinaryFormatter();
+
+                NameCard nc = new NameCard();
+                nc.Name = "Michael Jackson";
+                nc.Phone = "010-1234-5678";
+                nc.Age = 40;
+
+                serializer.Serialize(ws, nc);
+            }
+
+            using Stream rs = new FileStream("a.dat", FileMode.Open);
+            BinaryFormatter deserializer = new BinaryFormatter();
+
+            NameCard nc2;
+            nc2 = (NameCard)deserializer.Deserialize(rs);
+
+            Console.WriteLine($"Name:  {nc2.Name}");
+            Console.WriteLine($"Phone: {nc2.Phone}");
+            Console.WriteLine($"Age:   {nc2.Age}");
+        }
+    }
+}
---
+++ chapter_18/SerializingCollection/MainApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializingCollection
+{
+    [Serializable]
+    class NameCard
+    {
+        public NameCard(string Name, string Phone, int Age)
+        {
+            this.Name = Name;
+            this.Phone = Phone;
+            this.Age = Age;
+        }
+
+        public string Name;
+        public string Phone;
+        public int Age;
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            using (Stream ws = new FileStream("a.dat", FileMode.Create))
+            {
+                BinaryFormatter serializer = new BinaryFormatter();
+
+                List<NameCard> list = new List<NameCard>();
+                list.Add(new NameCard("Micahel Jackson", "010-0000-0000", 40));
+                list.Add(new NameCard("Bob Marley", "010-1111-1111", 40));
+                list.Add(new NameCard("Lady Gaga", "010-2222-2222", 40));
+
+                serializer.Serialize(ws, list);
+            }
+
+            using Stream rs = new FileStream("a.dat", FileMode.Open);
+            BinaryFormatter deserializer = new BinaryFormatter();
+
+            List<NameCard> list2;
+            list2 = (List<NameCard>)deserializer.Deserialize(rs);
+
+            foreach(NameCard nc in list2)
+            {
+                Console.WriteLine(
+                    $"Name: {nc.Name}, Phone: {nc.Phone}, Age: {nc.Age}");
+            }
+        }
+    }
+}
